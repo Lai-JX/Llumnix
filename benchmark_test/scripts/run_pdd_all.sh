afa0fc5d87acfc9720558cb44a704fa8b21e7233
Lai-JX <2840026059@qq.com>
2025-07-04 14:40:27 +0000

    change function:get_required_migration_request,

--- benchmark_test/scripts/run_pdd_all.sh
+++ benchmark_test/scripts/run_pdd_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 req_num=2000
-gputype=L40-multi-port-zmp-parallel-test
+gputype=L40-multi-port-zmp-parallel-test-2
 prefix="$gputype-pdd-hetero"
 ./run_pdd_multi.sh 1 $req_num 4 llama-7b poisson 6 $gputype
 # ./tp_heterogeneity.sh "1,1" "2" $req_num llama-7b poisson 6 $prefix
